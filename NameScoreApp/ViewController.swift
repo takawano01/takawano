90c59cabd3b0fdb410de5832231c4ec618d3a27d
takawano <ta_kawano@mac.com>
2020-07-17 15:38:25 +0900

    Initial Commit

---
+++ NameScoreApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NameScoreApp
+//
+//  Created by 河野隆 on 2020/07/17.
+//  Copyright © 2020 河野隆. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ NameScoreAppTests/NameScoreAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  NameScoreAppTests.swift
+//  NameScoreAppTests
+//
+//  Created by 河野隆 on 2020/07/17.
+//  Copyright © 2020 河野隆. All rights reserved.
+//
+
+import XCTest
+@testable import NameScoreApp
+
+class NameScoreAppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
