0c4ec9dd3c50450106bb6cd1cceef99914b1db52
takawano <ta_kawano@mac.com>
2020-07-19 13:22:34 +0900

    ドットインストールの姓名判断アプリです

---
+++ NameScoreApp/ResultViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ResultViewController.swift
+//  NameScoreApp
+//
+//  Created by 河野隆 on 2020/07/18.
+//  Copyright © 2020 河野隆. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    
+    var myName: String = ""
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print(myName)
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- NameScoreApp/ViewController.swift
+++ NameScoreApp/ViewController.swift
@@ -9,12 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var nameText: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let identifier = segue.identifier else {
+            return
+        }
+        if identifier == "showReslt" {
+            let resultVC = segue.destination as! ResultViewController
+            resultVC.myName = self.nameText.text!
+        }
+    }
 }
 
